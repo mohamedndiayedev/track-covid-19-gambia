7f1d98f2cbc2285fab973e51788b3445c4dc48e2
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-08-20 07:21:29 -1200

    Update setupTests.js

--- admin/src/setupTests.js
+++ admin/src/setupTests.js
@@ -2,5 +2,5 @@ require('jest-fetch-mock').enableMocks()
 
 process.env.REACT_APP_NAME = 'TrackCOVID'
 process.env.REACT_APP_WEB_APP_DOMAIN = 'https://track-covid-19-gambia.herokuapp.com'
-process.env.REACT_APP_SERVER_DOMAIN = 'https://track-covid-19-gambia.herokuapp.com/'
+process.env.REACT_APP_SERVER_DOMAIN = 'https://track-covid-19-gambia.herokuapp.com'
 process.env.REACT_APP_REGISTRATION_URL = 'http://my-form.my-org'
