7bea8746c75c5fbf57a23747ee50b5233c81ed6a
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-08-20 05:25:16 -1200

    Update setupTests.js

--- app/src/setupTests.js
+++ app/src/setupTests.js
@@ -1,8 +1,8 @@
 process.env.REACT_APP_NAME = 'ANK Analytics - Covid-19 Exposure App (REPUBLIC OF THE GAMBIA)'
 process.env.REACT_APP_THEME = 'dark'
 process.env.REACT_APP_ABOUT_URL = 'https://track-covid-19-gambia.herokuapp.com'
-process.env.REACT_APP_ADMIN_DOMAIN = 'https://trackcovid.net'
-process.env.REACT_APP_SERVER_DOMAIN = 'https://trackcovid.net'
+process.env.REACT_APP_ADMIN_DOMAIN = 'https://demo.trackcovid.net'
+process.env.REACT_APP_SERVER_DOMAIN = 'https://demo.trackcovid.net'
 process.env.REACT_APP_ESTIMATED_DX_DELAY_DAYS = 7
 process.env.REACT_APP_CONTACT_WINDOW_HOURS_BEFORE = 1
 process.env.REACT_APP_CONTACT_WINDOW_HOURS_AFTER = 2
